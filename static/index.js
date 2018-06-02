74920f960f2d2ba5f2c41fc46ae5426f5110f396
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2018-06-02 09:20:13 -0400

    Fix lint

--- static/index.js
+++ static/index.js
@@ -3,7 +3,7 @@ const Joy = require('../src/joy/joy')
 const inputEl = document.querySelector('[name=input]')
 const outputEl = document.querySelector('.output-result')
 const history = []
-let pointer
+// let pointer
 
 function onInputKeydown (e) {
   if (e.key === 'Enter') {
@@ -13,7 +13,7 @@ function onInputKeydown (e) {
       const result = Joy().run(input)
       outputEl.textContent = result
       history.push(input)
-      pointer = history.length - 1
+      // pointer = history.length - 1
       inputEl.value = ''
     } catch (e) {
       outputEl.textContent = e.toString()
