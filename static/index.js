adfdb37d44722b97edf63c2b09a17ab650d0f083
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2018-06-01 23:15:46 -0400

    Initial static site (wip)

---
+++ static/index.js
@@ -0,0 +1,24 @@
+const Joy = require('../src/joy/joy')
+
+const inputEl = document.querySelector('[name=input]')
+const outputEl = document.querySelector('.output-result')
+const history = []
+let pointer
+
+function onInputKeydown (e) {
+  if (e.key === 'Enter') {
+    e.preventDefault()
+    try {
+      const input = inputEl.value.trim()
+      const result = Joy().run(input)
+      outputEl.textContent = result
+      history.push(input)
+      pointer = history.length - 1
+      inputEl.value = ''
+    } catch (e) {
+      outputEl.textContent = e.toString()
+    }
+  }
+}
+
+inputEl.addEventListener('keydown', onInputKeydown)
